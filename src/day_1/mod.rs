bcbcdcb2d6fd2f22952520d4f58f2d7a88fda24b
Jun Ling <jun@insight.co>
2021-12-05 10:46:16 +1100

    Implement solution for first exercise

--- src/day_1/mod.rs
+++ src/day_1/mod.rs
@@ -1,9 +1,42 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 
-use std::fs;
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+  P: AsRef<Path>,
+{
+  let file = File::open(filename)?;
+  Ok(io::BufReader::new(file).lines())
+}
 
+// https://adventofcode.com/2021/day/1
 pub fn task_one() {
-  let contents = fs::read_to_string("resources/day-1.txt")
-  .expect("Something went wrong reading the file");
+  let mut n_increments = 0;
+  let mut last: Option<i32> = None;
 
-  println!("With text:\n{}", contents);
+  match read_lines("./resources/day-1.txt") {
+    Ok(lines) => {
+      // Consumes the iterator, returns an (Optional) String
+      for line in lines {
+        if let Ok(n) = line {
+          match n.parse::<i32>() {
+            Ok(new_val) => {
+              if let Some(value) = last {
+                if new_val > value {
+                  n_increments += 1;
+                }
+              }
+
+              last = Some(new_val);
+            }
+            Err(_) => (),
+          }
+        }
+      }
+    }
+    Err(e) => println!("Failed to read file with err:{}", e),
+  }
+
+  println!("Number of increments: {}", n_increments);
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let day_num = &args[1];
 
     match day_num.as_str() {
-      "1" => day_1::task_one(),
-      _ => println!("Not done with that yet, try another number yo")
+        "1" => day_1::task_one(),
+        _ => println!("Not done with that yet, try another number yo"),
     }
 }
