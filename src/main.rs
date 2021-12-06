49a8e19aa047d8eec16d409d66c06e1e47e334a0
Jun Ling <jun@insight.co>
2021-12-06 17:32:32 +1100

    Implement second task

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,14 @@ mod day_1;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let day_num = &args[1];
+    let task_num = &args[2];
 
     match day_num.as_str() {
-        "1" => day_1::task_one(),
+        "1" => match task_num.as_str() {
+            "1" => day_1::task_one(),
+            "2" => day_1::task_two(),
+            _ => println!("No match for exercise {}, task {}", day_num, task_num),
+        },
         _ => println!("Not done with that yet, try another number yo"),
     }
 }
