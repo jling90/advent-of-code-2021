c696144d0442ac4ef04ae03288c1bcbdf1d335f7
Jun Ling <jun@insight.co>
2021-12-07 13:45:55 +1100

    Feat: day 2 (#3)
    * Implement solution for day 2 - 1
    
    * Implement solution for day 2 - 2

---
+++ src/day_2.rs
@@ -0,0 +1,56 @@
+use std::fs::File;
+use std::io::{self};
+
+pub fn task_one(lines: io::Lines<io::BufReader<File>>) -> i32 {
+  let mut x = 0;
+  let mut y = 0;
+
+  for line in lines {
+    if let Ok(text) = line {
+      let mut splits = text.split_whitespace();
+
+      // First split is the direction, second is the magnitude
+      if let (Some(direction), Some(magnitude_str)) = (splits.next(), splits.next()) {
+        if let Ok(magnitude) = magnitude_str.parse::<i32>() {
+          match direction {
+            "up" => x -= magnitude,
+            "down" => x += magnitude,
+            "forward" => y += magnitude,
+            _ => (),
+          }
+        }
+      }
+    }
+  }
+
+  return x * y;
+}
+
+pub fn task_two(lines: io::Lines<io::BufReader<File>>) -> i32 {
+  let mut aim = 0;
+  let mut x = 0;
+  let mut y = 0;
+
+  for line in lines {
+    if let Ok(text) = line {
+      let mut splits = text.split_whitespace();
+
+      // First split is the direction, second is the magnitude
+      if let (Some(direction), Some(magnitude_str)) = (splits.next(), splits.next()) {
+        if let Ok(magnitude) = magnitude_str.parse::<i32>() {
+          match direction {
+            "up" => aim -= magnitude,
+            "down" => aim += magnitude,
+            "forward" => {
+              x += magnitude;
+              y += magnitude * aim;
+            }
+            _ => (),
+          }
+        }
+      }
+    }
+  }
+
+  return x * y;
+}
