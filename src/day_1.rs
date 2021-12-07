bb8874c85be56c5a2d5f643c479683a054e399c9
Jun Ling <jun@insight.co>
2021-12-07 12:59:42 +1100

    Chore: rework proj structure (#2)
    * Unnest exercise structure
    
    * Move file-reading logic out of tasks, put in shared util

---
+++ src/day_1.rs
@@ -0,0 +1,73 @@
+use std::fs::File;
+use std::io::{self};
+
+/*
+ * https://adventofcode.com/2021/day/1
+ *
+ * Read through a file, check if last line (as int)
+ * is larger than the previous value and return number
+ * of times there was an increase.
+ */
+
+pub fn task_one(lines: io::Lines<io::BufReader<File>>) -> String {
+  let mut n_increments = 0;
+  let mut last: Option<i32> = None;
+
+  for line in lines {
+    if let Ok(n) = line {
+      if let Ok(new_val) = n.parse::<i32>() {
+        if let Some(value) = last {
+          if new_val > value {
+            n_increments += 1;
+          }
+        }
+
+        last = Some(new_val);
+      }
+    }
+  }
+
+  return n_increments.to_string();
+}
+
+/*
+ * https://adventofcode.com/2021/day/1#part2
+ *
+ * Like the previous task but using a sliding window of three.
+ */
+pub fn task_two(lines: io::Lines<io::BufReader<File>>) -> String {
+  let mut last_window_sum: Option<i32> = None;
+  let mut n_increments = 0;
+
+  let parsed_lines: Vec<i32> = lines
+    .filter_map(|line| {
+      if let Ok(n) = line {
+        if let Ok(new_val) = n.parse::<i32>() {
+          Some(new_val)
+        } else {
+          None
+        }
+      } else {
+        None
+      }
+    })
+    .collect();
+
+  let mut i = 0;
+
+  while i < parsed_lines.len() - 2 {
+    let current_window_sum: i32 = parsed_lines[i..i + 3].iter().sum();
+
+    if let Some(last_sum) = last_window_sum {
+      if current_window_sum > last_sum {
+        n_increments += 1;
+      }
+    }
+
+    last_window_sum = Some(current_window_sum);
+
+    i += 1;
+  }
+
+  return n_increments.to_string();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::env;
 mod day_1;
+mod util;
+
+const EX_1_FILE_PATH: &str = "./resources/day-1.txt";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,8 +11,20 @@ fn main() {
 
     match day_num.as_str() {
         "1" => match task_num.as_str() {
-            "1" => day_1::task_one(),
-            "2" => day_1::task_two(),
+            "1" => {
+                if let Ok(lines) = util::read_lines(EX_1_FILE_PATH) {
+                    println!("{}", day_1::task_one(lines));
+                } else {
+                    println!("Failed to read file");
+                }
+            }
+            "2" => {
+                if let Ok(lines) = util::read_lines(EX_1_FILE_PATH) {
+                    println!("{}", day_1::task_two(lines));
+                } else {
+                    println!("Failed to read file");
+                }
+            }
             _ => println!("No match for exercise {}, task {}", day_num, task_num),
         },
         _ => println!("Not done with that yet, try another number yo"),
