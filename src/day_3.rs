6a8ad40bef87452c29455459b9099abd209bd24c
Jun Ling <jun@insight.co>
2021-12-07 20:24:10 +1100

    Extract common logic into shared helper

--- src/day_3.rs
+++ src/day_3.rs
@@ -1,9 +1,14 @@
 use std::fs::File;
 use std::io::{self};
 
+// Number of bits for each line in the problem input
 const N_BITS: usize = 12;
 
-pub fn task_one(lines: io::Lines<io::BufReader<File>>) -> isize {
+// For a sequence of size N_BITS strings of 1s and 0s
+// Return as a tuple
+// - String with 1s in each position where 1 is most common.
+// - String with 0s in each position where 1 is most common.
+fn most_popular_bit_strings(lines: io::Lines<io::BufReader<File>>) -> (String, String) {
   // Threshold for determining whether '1s' are 'more common'
   // This is half the total number of lines.
   let mut midpoint: f32 = 0.0;
@@ -26,7 +31,7 @@ pub fn task_one(lines: io::Lines<io::BufReader<File>>) -> isize {
   }
 
   ones_counts.iter().for_each(|n| {
-    if *n as f32 > midpoint {
+    if *n as f32 >= midpoint {
       gamma.push('1');
       epsilon.push('0');
     } else {
@@ -34,6 +39,12 @@ pub fn task_one(lines: io::Lines<io::BufReader<File>>) -> isize {
       epsilon.push('1');
     }
   });
+
+  return (gamma, epsilon);
+}
+
+pub fn task_one(lines: io::Lines<io::BufReader<File>>) -> isize {
+  let (gamma, epsilon) = most_popular_bit_strings(lines);
 
   return isize::from_str_radix(&gamma, 2).unwrap() * isize::from_str_radix(&epsilon, 2).unwrap();
 }
