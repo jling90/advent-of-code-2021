7bb3c17df84132ad0a59a8da4dc5aab80878bade
Jun Ling <jun@insight.co>
2021-12-14 12:29:33 +1100

    Rework most_popular_bit_strings to take Vec<String>

--- src/day_3.rs
+++ src/day_3.rs
@@ -8,30 +8,26 @@ const N_BITS: usize = 12;
 // Return as a tuple
 // - String with 1s in each position where 1 is most common.
 // - String with 0s in each position where 1 is most common.
-fn most_popular_bit_strings(lines: io::Lines<io::BufReader<File>>) -> (String, String) {
+fn most_popular_bit_strings(lines: Vec<String>) -> (String, String) {
   // Threshold for determining whether '1s' are 'more common'
   // This is half the total number of lines.
-  let mut midpoint: f32 = 0.0;
+  let midpoint = lines.len() / 2;
   // Count of '1's in each position
-  let mut ones_counts = vec![0; N_BITS];
+  let ones_counts = lines.iter().fold(vec![0; N_BITS], |mut counts, line| {
+    for (index, bit) in line.char_indices() {
+      if bit == '1' {
+        counts[index] += 1
+      }
+    }
+
+    counts
+  });
 
   let mut gamma = "".to_owned();
   let mut epsilon = "".to_owned();
 
-  for line in lines {
-    if let Ok(bits) = line {
-      midpoint += 0.5;
-
-      for (index, bit) in bits.char_indices() {
-        if bit == '1' {
-          ones_counts[index] += 1
-        }
-      }
-    }
-  }
-
   ones_counts.iter().for_each(|n| {
-    if *n as f32 >= midpoint {
+    if *n >= midpoint {
       gamma.push('1');
       epsilon.push('0');
     } else {
@@ -44,7 +40,7 @@ fn most_popular_bit_strings(lines: io::Lines<io::BufReader<File>>) -> (String, S
 }
 
 pub fn task_one(lines: io::Lines<io::BufReader<File>>) -> isize {
-  let (gamma, epsilon) = most_popular_bit_strings(lines);
+  let (gamma, epsilon) = most_popular_bit_strings(lines.map(|l| l.unwrap()).collect());
 
   return isize::from_str_radix(&gamma, 2).unwrap() * isize::from_str_radix(&epsilon, 2).unwrap();
 }
