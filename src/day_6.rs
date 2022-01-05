c179b9c8484886f72e4c129977f88455445661f8
Jun Ling <jun@insight.co>
2022-01-05 17:27:09 +1100

    Implement more efficient solution for 6-1, extract logic into helper

--- src/day_6.rs
+++ src/day_6.rs
@@ -1,25 +1,46 @@
 use std::fs::File;
 use std::io::{self};
 
-pub fn task_one(mut lines: io::Lines<io::BufReader<File>>) -> usize {
-  let mut timers: Vec<u32> = lines
+fn get_timers(mut lines: io::Lines<io::BufReader<File>>, epoch: u8) -> i32 {
+  let mut timers = lines
     .next()
     .unwrap()
     .unwrap()
     .split(",")
-    .filter_map(|s| s.parse().ok())
-    .collect();
-
-  for _ in 0..80 {
-    for i in 0..timers.len() {
-      if timers[i] > 0 {
-        timers[i] -= 1;
-      } else {
-        timers[i] = 6;
-        timers.push(8);
+    .fold(vec![0; 10], |mut accum, s| {
+      if let Ok(timer) = s.parse::<usize>() {
+        accum[timer] += 1;
       }
+
+      accum
+    });
+
+  for _ in 0..epoch {
+    let mut updates: Vec<i32> = vec![0; 10];
+
+    // Iterate backwards so new items only
+    // decrement on next loop.
+    for i in (0..timers.len()).rev() {
+      if i == 0 {
+        updates[8] += timers[i];
+        updates[6] += timers[i];
+        updates[0] -= timers[i];
+      } else {
+        if timers[i] > 0 {
+          updates[i] -= timers[i];
+          updates[i - 1] += timers[i];
+        }
+      }
+    }
+
+    for i in 0..timers.len() {
+      timers[i] += updates[i]
     }
   }
 
-  timers.len()
+  timers.iter().sum()
+}
+
+pub fn task_one(lines: io::Lines<io::BufReader<File>>) -> i32 {
+  get_timers(lines, 80)
 }
