d5aecaf915e7c3b85295b361628a4b0ba43003e6
Jun Ling <jun@insight.co>
2022-01-06 15:08:45 +1100

    Simplify day 6 solution

--- src/day_6.rs
+++ src/day_6.rs
@@ -7,7 +7,7 @@ fn get_timers(mut lines: io::Lines<io::BufReader<File>>, epoch: u16) -> u64 {
     .unwrap()
     .unwrap()
     .split(",")
-    .fold(vec![0; 10], |mut accum, s| {
+    .fold(vec![0; 9], |mut accum, s| {
       if let Ok(timer) = s.parse::<usize>() {
         accum[timer] += 1;
       }
@@ -16,29 +16,11 @@ fn get_timers(mut lines: io::Lines<io::BufReader<File>>, epoch: u16) -> u64 {
     });
 
   for _ in 0..epoch {
-    let mut updates: Vec<i64> = vec![0; 10];
-
-    // Iterate backwards so new items only
-    // decrement on next loop.
-    for i in (0..timers.len()).rev() {
-      if i == 0 {
-        updates[8] += timers[i];
-        updates[6] += timers[i];
-        updates[0] -= timers[i];
-      } else {
-        if timers[i] > 0 {
-          updates[i] -= timers[i];
-          updates[i - 1] += timers[i];
-        }
-      }
-    }
-
-    for i in 0..timers.len() {
-      timers[i] += updates[i]
-    }
+    timers.rotate_left(1);
+    timers[6] += timers[8];
   }
 
-  timers.iter().map(|n| *n as u64).sum()
+  timers.iter().sum()
 }
 
 pub fn task_one(lines: io::Lines<io::BufReader<File>>) -> u64 {
