a4b8ad9e6f885d767ffcff808eb0e8b9e5a5785c
Jun Ling <jun@insight.co>
2022-01-09 13:38:30 +1100

    Add solution for 7-2

--- src/main.rs
+++ src/main.rs
@@ -132,6 +132,13 @@ fn main() {
                     println!("Failed to read file");
                 }
             }
+            "2" => {
+                if let Ok(lines) = util::read_lines(EX_7_FILE_PATH) {
+                    println!("{}", day_7::task_two(lines));
+                } else {
+                    println!("Failed to read file");
+                }
+            }
             _ => println!("No match for exercise {}, task {}", day_num, task_num),
         },
         _ => println!("Not done with that yet, try another number yo"),
