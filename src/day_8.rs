01ab6905c05eaffd6fc64ce37555155d918cf255
Jun Ling <jun@insight.co>
2022-01-10 13:55:58 +1100

    Add solution for 8-1

---
+++ src/day_8.rs
@@ -0,0 +1,36 @@
+use std::fs::File;
+use std::io::{self};
+
+fn input_to_pairs(lines: io::Lines<io::BufReader<File>>) -> Vec<(Vec<String>, Vec<String>)> {
+  lines
+    .filter_map(|line| line.ok())
+    .map(|line| {
+      let mut splits = line.split("|");
+
+      let (signal_pattern, outputs) = (splits.next().unwrap(), splits.next().unwrap());
+
+      (
+        signal_pattern
+          .split_whitespace()
+          .map(|s| s.to_string())
+          .collect(),
+        outputs.split_whitespace().map(|s| s.to_string()).collect(),
+      )
+    })
+    .collect()
+}
+
+pub fn task_one(lines: io::Lines<io::BufReader<File>>) -> usize {
+  input_to_pairs(lines).iter().fold(0, |sum, (_, outputs)| {
+    sum
+      + outputs.iter().fold(0, |s, o| {
+        s + match o.chars().count() {
+          2 => 1,
+          3 => 1,
+          4 => 1,
+          7 => 1,
+          _ => 0,
+        }
+      })
+  })
+}
